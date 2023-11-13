dcd8f769bdfd30162f10cc66a643c01dfdd1f741
Matheus Pacheco Cruz <pachecx35@gmail.com>
2023-11-13 17:47:04 -0300

    up

--- Arrays/main.js
+++ Arrays/main.js
@@ -4,7 +4,6 @@ console.log(num.length)
 console.log(num[1])
 console.log(num[6])
 
-
 const text = 'testando'
 console.log(text.length)
 
@@ -15,7 +14,7 @@ console.log(num.concat(ar1)) //Unir dois arrays
 const text2 = "Lorem iusum Ipisum lorem"
 console.log(text2.toLocaleUpperCase())
 console.log(text2.toLowerCase())
-console.log(text2.indexOf('m')) //Encontrar a posição de um caractere 
+console.log(text2.indexOf('m')) //Encontrar a posição de um caractere
 
 //Objetos
 const person = {
@@ -45,8 +44,135 @@ delete car.modelo
 console.log(car)
 
 //Mais sobre Objetos
-const 
+const obj = {
+    a: 'teste',
+    b: 33,
+    c: true
+}
+const obj2 = {}
+
+Object.assign(obj2, obj) //Copiando propiedades do primeiro para o segundo
+console.log(obj2)
+
+console.log(Object.keys(obj)) //Obter as chaves do objeto
+console.log(Object.keys(obj2))
+console.log(Object.keys(car))
+
+console.log(Object.entries(obj2)) //Obter chave e valor
 
 
+//Mutação
+const a = {
+    name: 'matheus'
+}
+
+const b = a;
+
+console.log(b)
+
+// delete a.name
+// console.log(b)
+
+delete b.name
+console.log(a);
+
+//Loops em Arryas
+const users = ['matheus', 'pedro', 'lucas', 'joao']
+for (let i = 0; i < users.length; i++) {
+    console.log(`Lista de usuarios: ${users[i]}`)
+};
+
+//Push e Pop
+const p = ['a', 'b', 'c', 'd'];
+const O = [1, 2, 3, 4, 5];
+p.push(55, 66, 100, 'Cem');   //Adiciona no final 
+console.log(p);
+
+O.pop()  //Remove o ultimo
+console.log(O);
+console.log(O.length);
+
+//Shift e UnShift
+const Z = ['a', 'b', 'c', 'd']; //Adiciona no inicio
+const X = [1, 2, 3, 4, 5];
+
+Z.unshift(22, 25, 'dois')
+console.log(Z)
+X.shift()
+console.log(X)
+
+//IndexOf LastIndexOF 
+const M = ['a', 'b', 'c', 'd', 'Z']
+
+
+console.log(M.indexOf('Z'))
+console.log(M.lastIndexOf('a'))
+
+//Slice
+const S = ['a', 'b', 'c', 'd', 'Z'] // Fragmentar o array em outro menor sem modificar o original
+const s = S.slice(1, 3 + 1)
+console.log(s)
+
+//ForEach
+const numbers = [1, 2, 3, 4, 5, 6]
+numbers.forEach((numero)=>{
+    console.log(`O numero é: ${numero}`)
+})
+
+const posts = [
+    {title: 'primeiro post', categoria: 'PHP'},
+    {title: 'Segundo post', categoria: 'JS'},
+    {title: 'terceiro post', categoria: 'Ruby'},
+    {title: 'quarto post', categoria: 'Java'},
+]
+
+posts.forEach((post)=>{
+    console.log(`Exibir post: ${post.title}, da categoria: ${post.categoria}`)
+})
+
+//Includes
+const I = ['pamonha', 'geleia', 'paçoca']
+console.log(I.includes('geleia'))
+console.log(I.includes('boi'))
+console.log(I.includes('paçoca'))
+
+//Reverse
+const R = ['teste do testado testando 22', 50 , 'novo teste']
+console.log(R.reverse())
+
+//Métodos em Strings
+
+//TRIM
+const T = '          removendo espaçamento do texto'
+console.log(T.trim(''))
+
+//PadStart
+const P = '12'
+console.log(P.padStart(4,'00'))
+console.log(P.padEnd(4,'00'))
+
+//Split
+const stri = 'futuramente' 
+console.log(stri.split(''))
+
+//Join
+console.log(stri.split('').join(''))
+
+//Repeat
+const r = 'fumaça'
+console.log(r.repeat(12))
+
+//Rest Operator
+const somaInfinita = (...args) => {
+    let total = 10;
+
+    for (let i = 0; i < args.length; i++) {
+        total += args[i]
+    }
+    return total
+}
+console.log(somaInfinita(2, 5, 14, 45, 155))
+console.log(somaInfinita(289, 5, 144, 45, 155))
+console.log(somaInfinita(286649, 4645, 16644, 4465, 3615585))
 
 
