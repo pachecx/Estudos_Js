eb54493472048aea0919a42307eec279e5cbc037
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-01-27 10:30:54 -0300

    Mais sobre tipos de dados

---
+++ 1 - Tipos De Dados/main.js
@@ -0,0 +1,48 @@
+let a = "teste";
+const b = 36;
+const c = true;
+const d = 1 + "2";
+const e = 2 * "2";
+
+console.log(typeof a, typeof b, typeof c);
+console.log(d, typeof d);
+console.log(e, typeof e);
+
+//Especial NuMBER
+
+console.log(typeof(Infinity, NaN, -Infinity))
+
+//String
+console.log('texto aqui usando aspas simples');
+console.log("texto aqui aspas duplas");
+console.log(typeof(`texto aqui usando template literals`));
+                                                //Quebra de linha na STRING
+console.log('primeira linha \nsegunda linha');  //Tem que colocar o /n pr acontecer a quebra
+console.log('MAtheus \nPacheco');
+
+console.log(`A soma entre 5 e 3 é: ${5 + 3}.`)
+
+//BOOLEANs;
+console.log(true);
+console.log(false);
+console.log((2>20));
+console.log(2>20);
+console.log(200>20);
+console.log(2 == '2');
+console.log(2 === '2');
+console.log(5 >= 1);
+console.log(2 != '2');
+console.log(2 !== '2');
+
+console.log(typeof (200>20));
+
+//BOOLEANs: Operadores Logicos
+
+// && os dois lados precisam ser true
+// || Basta um lado ser TRUe 
+// !- Inverte os valor de true para  false ou false para true
+
+
+
+
+
