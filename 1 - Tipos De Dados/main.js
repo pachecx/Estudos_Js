74d37a3f095c912326e6f9b6bc357cef71b762f1
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-01-28 09:38:11 -0300

    finalizando tipos de dados

--- 1 - Tipos De Dados/main.js
+++ 1 - Tipos De Dados/main.js
@@ -9,7 +9,6 @@ console.log(d, typeof d);
 console.log(e, typeof e);
 
 //Especial NuMBER
-
 console.log(typeof(Infinity, NaN, -Infinity))
 
 //String
@@ -42,7 +41,46 @@ console.log(typeof (200>20));
 // || Basta um lado ser TRUe 
 // !- Inverte os valor de true para  false ou false para true
 
+console.log((5 > 3) && (4 > 1));
+console.log((5 > 30) && (4 > 1));
 
+console.log((5 > 3) || (4 > 1));
+console.log((5 > 30) || (4 > 1));
 
+console.log(!true)
 
+//Operador ternario
+console.log(5 > 2 ? 'Sim': 'Não'); //Primeiro a pergunta, depois o IF e depois ELSE
+console.log(1 == '1'? 'É true': "false")
+console.log(true ? 10 : 1)
+
+//Empty Values
+console.log(null);
+console.log(undefined);
+
+//Exercicios Tipos de dados
+//01
+console.log('String usando aspas simples');
+console.log("String usando aspas duplas");
+console.log(`String usando template literals`);
+
+//02
+console.log(5);
+console.log(5.6);
+console.log(5 + 6);
+
+//03
+console.log(5 > 3);
+console.log( 5 < 3);
+console.log( 5 == 3);
+console.log( 5 === 3);
+console.log( 5 != 3);
+
+//04
+console.log((1 > 2) && (1 == 1));
+console.log((1 > 2) || (1 == 1));
+console.log(!true);
+
+//05
+console.log('cinco' * 5);
 
