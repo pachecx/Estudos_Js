c3e06f0fbe22945be3e9024f52341c063a5e69d8
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-01-29 16:59:01 -0300

    iniciando Estrutura de programação

---
+++ 2-Estrutura de Programação/main.js
@@ -0,0 +1,23 @@
+let nome = "Matheus";
+console.log(`O meu nome é ${nome}`);
+
+console.log(Math.max(8, 45, 87,846,48, 451,200, 36, 489, 848));
+console.log(Math.min(8, 45, 87,846,48, 451,200, 36, 489, 848));
+
+let idade = 17;
+if(idade < 18){
+    console.log('Barrado')
+}
+
+if(idade >= 18){
+    console.log('Liberado!')
+}else{
+    console.log('Barrado!')
+}
+
+let number = 10;
+
+while (number > 0) {
+    console.log(number)
+    number --
+};
