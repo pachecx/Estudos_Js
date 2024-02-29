b2cbea07af00f42e8b998e33ba5d6b70fe214218
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-02-29 08:32:20 -0300

    up

---
+++ 9-Js no Navegador/main.js
@@ -0,0 +1,18 @@
+console.log(document);
+console.log(document.body.childNodes);
+
+console.log(document.body.childNodes[1]);
+console.log(document.body.childNodes[2]);
+console.log(document.getElementsByTagName("h1"));
+
+console.log(document.getElementById("D"));
+console.log(document.getElementsByClassName("A"));
+
+console.log(document.querySelector("#D"));
+console.log(document.querySelector(".A"));
+
+let elemento = document.querySelector(".A")
+
+console.log(elemento.style.color = 'red');
+
+
